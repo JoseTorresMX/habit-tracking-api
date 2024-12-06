91980aa4519c8c5e4418ed9019575889c93f20d9
JoseTorres <joseangel.tosa@outlook.com>
2024-12-05 22:47:24 -0700

    conf app

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ from flask_restx import Api, Resource, fields, reqparse
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
 
 # Construyendo la app
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = 'super-secret'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
