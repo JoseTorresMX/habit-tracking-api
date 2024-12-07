39d64003d2b3a25fc896cb1d8305c23f6e27262c
JoseTorres <joseangel.tosa@outlook.com>
2024-12-07 10:45:58 -0700

    restructura 07122024

--- doc/app/app v1/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-class Config:
-    MYSQL_HOST = 'localhost' 
-    MYSQL_USER = 'jtorres'  
-    MYSQL_PASSWORD = '123456789'  
-    MYSQL_DB = 'habit_tracker'
--- doc/app/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-# routes/__init__.py
-
-from .user_routes import user_routes
-from .habit_routes import habit_routes
