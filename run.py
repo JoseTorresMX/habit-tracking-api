90ade05d8ae80294dac51eafdc835abe015573cb
JoseTorres <joseangel.tosa@outlook.com>
2024-12-07 19:51:34 -0700

    funinal

--- run.py
+++ run.py
@@ -7,3 +7,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # creat las tablas si no existen
     app.run(debug=True)  # correr el server en modo desarrollo
+# Funcional
